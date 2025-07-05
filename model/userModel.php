8b5758607f1a0fe5ed8252b5b71808eca1d60960
Daniel <danielnctn@gmail.com>
2025-07-05 00:32:36 -0300

    ADD versão inicial model; conexão com o banco de dados através do mysqli e criação dos métodos ainda sem conteúdo

---
+++ model/userModel.php
@@ -0,0 +1,75 @@
+<?php
+
+class userModel
+{
+    private static $instance = null; // instância da classe
+    private $con = null; // conexão do BD
+
+    public static function getInstance(): userModel
+    {
+        if (self::$instance == null) {
+            self::$instance = new userModel();
+            self::$instance->initDatabase();
+        }
+
+        return self::$instance;
+    }
+
+    private function initDatabase()
+    {
+        try {
+            $this->con = mysqli_init();
+            $this->con->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5);
+            if (!$this->con->real_connect("localhost", "root", "admin", 'publico')) {
+                $this->con = null;
+                throw new Exception("Falha ao conectar no banco de dados.");
+            }
+            //if (!$this->con->query("CREATE TABLE IF NOT EXISTS users(id INT(5) AUTO_INCREMENT, fullname TEXT(200), PRIMARY KEY (id))")) {
+            //    throw new Exception("Falha ao criar a tabela de números");
+            //  }
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    public function getUser($id = null): array|bool
+    {
+        if (!$this->con) {
+            throw new Exception("Conexão com o banco de dados não está aberta! Tente novamente.");
+        }
+
+        return [];
+    }
+
+    public function insertUser($data = []): bool|int
+    {
+        return true;
+    }
+
+    public function updateUser($data = []): bool
+    {
+        return true;
+    }
+
+
+    public function deleteUser($id): bool
+    {
+        return true;
+    }
+
+    public function deleteInstance()
+    {
+        // fechar conexão do BD antes
+        self::$instance = null;
+    }
+
+    public function __destruct()
+    {
+        if ($this->con) {
+            $this->con->close();
+        }
+        // fechar instancia do banco de dados
+    }
+}
+
+?>
